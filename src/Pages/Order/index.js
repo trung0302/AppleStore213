05c240cf775999e5f9cddf71a97940bd23e2467d
Trung <thanhtrungvnger@gmail.com>
2023-03-17 23:39:14 +0700

    add tailwind css

---
+++ src/Pages/Order/index.js
@@ -0,0 +1,13 @@
+// import styles from "./Order.module.css";
+
+function Order() {
+    return (
+        <div>
+            <div className="text-center text-6xl">
+                Trung
+            </div>
+        </div>
+    );
+}
+
+export default Order;
