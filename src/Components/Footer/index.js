6a0a1c09f318c0e8973adad16022267ef16dead0
photonrays <lthang052@gmail.com>
2023-03-18 15:48:25 +0700

    finished header

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,5 +1,22 @@
+import classes from "./Footer.module.css"
+
+
 function Footer() {
-    return <div>Footer</div>;
+    return <div className={classes.container}>
+        <div className={classes.footerUpper + " grid grid-cols-4 gap-4"}>
+            <div className="text-[#fff]">01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>01</div>
+            <div>09</div>
+        </div>
+        <div className={classes.footerLower}></div>
+    </div>;
 }
 
 export default Footer;
