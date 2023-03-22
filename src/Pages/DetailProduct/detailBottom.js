0611abb217ae447701058a1405b3d44da49b7261
nhung20521725 <pthnhunggt@gmail.com>
2023-03-22 15:39:27 +0700

    addDetailBottom

---
+++ src/Pages/DetailProduct/detailBottom.js
@@ -0,0 +1,4 @@
+function DetailBottom() {
+    return <div>Detail Product Bottom</div>;
+}
+export default DetailBottom;
--- src/Pages/DetailProduct/index.js
+++ src/Pages/DetailProduct/index.js
@@ -1,5 +1,11 @@
+import DetailBottom from "./detailBottom";
 function DetailProduct() {
-    return <div>Detail Product</div>;
+    return <div>
+        Detail Product
+        <div>
+            <DetailBottom/>
+        </div>
+    </div>;
 }
 
 export default DetailProduct;
