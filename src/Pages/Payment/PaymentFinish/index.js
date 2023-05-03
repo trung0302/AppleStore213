3ffad54fe359eec24a029b37cc0b017cf692e4bc
gameitthoi <lamdat3302@gmail.com>
2023-05-03 18:36:14 +0700

    thanh toan zalopay

--- src/Pages/Payment/PaymentFinish/index.js
+++ src/Pages/Payment/PaymentFinish/index.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import PaymentConfirm from "../Component/PaymentConfirm";
 
@@ -6,12 +5,12 @@ import PaymentConfirm from "../Component/PaymentConfirm";
 function PaymentFinish () {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
-
     const phuongThuc = searchParams.get("phuongthuc");
+    const amount = searchParams.get("amount");
+    //momo
     const partnerCode = searchParams.get("partnerCode");
     const orderId = searchParams.get("orderId");
     const requestId = searchParams.get("requestId");
-    const amount = searchParams.get("amount");
     const orderInfo = searchParams.get("orderInfo");
     const orderType = searchParams.get("orderType");
     const transId = searchParams.get("transId");
@@ -21,6 +20,15 @@ function PaymentFinish () {
     const responseTime = searchParams.get("responseTime");
     const extraData = searchParams.get("extraData");
     const signature = searchParams.get("signature");
+    //zalo
+    const appid = searchParams.get("appid");
+    const apptransid = searchParams.get("apptransid");
+    const discountamount = searchParams.get("discountamount");
+    const checksum = searchParams.get("checksum");
+    const pmcid = searchParams.get("pmcid");
+    const bankcode = searchParams.get("bankcode");
+    const status = searchParams.get("status");
+
 
     return (
         <div className="text-center text-2xl py-20">
@@ -31,10 +39,10 @@ function PaymentFinish () {
                     else
                         return(<PaymentConfirm method={phuongThuc} status={"thất bại"} madonhang={transId}/>)
                 else
-                    if(resultCode==1)
-                        return(<PaymentConfirm method={phuongThuc} status={"thành công"} madonhang={transId}/>)
+                    if(status==1)
+                        return(<PaymentConfirm method={phuongThuc} status={"thành công"} madonhang={apptransid}/>)
                     else
-                        return(<PaymentConfirm method={phuongThuc} status={"thành công"} madonhang={transId}/>)
+                        return(<PaymentConfirm method={phuongThuc} status={"thất bại"} madonhang={apptransid}/>)
             })()
             }
         </div>
