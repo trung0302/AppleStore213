f65cf3c662f1852e37f3470da8a656fb5840c6b9
photonrays <lthang052@gmail.com>
2023-05-11 22:31:56 +0700

    add user's menu

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,10 +4,28 @@ import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import { useState, useEffect, useRef } from 'react';
 import logo from '../../assets/image/logo1.jpg';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import {Login, Logout, Settings} from '@mui/icons-material'
 
 function Header() {
     const [search, setSearch] = useState(1);
     const [inputIsVisible, setInputIsVisible] = useState(false);
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     const inputRef = useRef(null);
 
@@ -79,7 +97,76 @@ function Header() {
             <a href='/cart'>
                 <ShoppingBagOutlinedIcon style={{ color: "#fff", fontSize: "28px" }} />
             </a>
-            <PersonOutlinedIcon style={{ color: "#fff", fontSize: "28px" }} />
+            <Tooltip title="Account settings">
+                <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    sx={{ ml: -0.2 }}
+                    aria-controls={open ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                >
+                    <PersonOutlinedIcon style={{ color: "#fff", fontSize: "28px" }} />
+                </IconButton>
+            </Tooltip>
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                        '&:before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: 'background.paper',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                        },
+                    },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                <MenuItem onClick={handleClose} sx={{fontSize: 15}}>
+                    <Avatar /> Trang cá nhân
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose} sx={{fontSize: 15}}>
+                    <ListItemIcon>
+                        <Settings fontSize="large" />
+                    </ListItemIcon>
+                    Cài đặt
+                </MenuItem>
+                <MenuItem onClick={handleClose} sx={{fontSize: 15}}>
+                    <ListItemIcon>
+                        <Login fontSize="large" />
+                    </ListItemIcon>
+                    Đăng nhập
+                </MenuItem>
+                <MenuItem onClick={handleClose} sx={{fontSize: 15}}>
+                    <ListItemIcon>
+                        <Logout fontSize="large" />
+                    </ListItemIcon>
+                    Thoát
+                </MenuItem>
+            </Menu>
         </div>
     </div>;
 }
